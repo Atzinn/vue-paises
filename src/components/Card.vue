1fc2ba1e0584fa400a4b88c1594ed10cf8e5aa71
Atzinn <atzinn.herrera@hotmail.com>
2021-06-18 13:30:52 -0500

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card mb-4 border border-info">
+      <div class="card-body bg-info">
+          <h5 class="text-center h3">{{pais.name}}</h5>
+          <p class="text-center">
+              <img :src="pais.flag" class="img-fluid w-25" :alt="`bandera de ${pais.name}`">
+          </p>
+          <p class="card-text">
+              <span class="badge badge-dark d-block mb-1">Native Name: {{pais.nativeName}}</span>
+              <span class="badge badge-dark d-block mb-1">Capital: {{pais.capital}}</span>
+              <span class="badge badge-dark d-block mb-1">Region: {{pais.region}}</span>
+              <span class="badge badge-info p-3 d-block mb-1">Population: {{numeroFormateado(pais.population)}}</span>
+          </p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['pais'],
+    setup() {
+        const numeroFormateado = (n) =>  new Intl.NumberFormat("de-DE").format(n)
+        return {
+            numeroFormateado
+        }
+    }
+}
+</script>
---
+++ src/components/Continentes.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="text-center">
+        <h3>Filtrar por continentes</h3>
+        <div class="btn-group mb-3">
+            <button class="btn btn-dark" @click="filtro('Americas')">AME</button>
+            <button class="btn btn-dark" @click="filtro('Europe')">EUR</button>
+            <button class="btn btn-dark" @click="filtro('Asia')">ASI</button>
+            <button class="btn btn-dark" @click="filtro('Oceania')">OCE</button>
+            <button class="btn btn-dark" @click="filtro('Africa')">AFR</button>
+            <button class="btn btn-dark" @click="filtro('')">ALL</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import { useStore } from 'vuex'
+export default {
+    setup() {
+        const store = useStore()
+        
+        const filtro = (region) => {
+            store.dispatch('filtrarRegion',region)
+        }
+
+        return {
+            filtro
+        }
+    }
+}
+</script>
